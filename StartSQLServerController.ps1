a92ff6beb58ea952b367ffaab9cffd0cb8f74ca9
Jim <jim@mouseconcepts.com>
2024-09-15 17:18:55 -0700

    Update StartSQLServerController.ps1
    Added documentation within code

--- StartSQLServerController.ps1
+++ StartSQLServerController.ps1
@@ -1,2 +1,13 @@
-﻿# Save this as StartSQLServerController.ps1
+#***********************************
+#*** Hide the PowerShell window  ***
+#***********************************
+#
+#
 Start-Process powershell -ArgumentList "-NoProfile -WindowStyle Hidden -File `"$PSScriptRoot\SQLServerController.ps1`""
+#
+#
+#***********************************
+#*** End hide PowerShell window  ***
+#***********************************
+#
+# SQL Server Services Controller © 2024 by Jim Calano is licensed under CC BY-NC-SA 4.0 
