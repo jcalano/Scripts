05822ae445e46f764edfec04ccdc116ff19b17cd
Jim <jim@mouseconcepts.com>
2024-09-15 17:19:57 -0700

    Update StartSqlServiceController.cmd
    Added documentation within the code

--- StartSqlServiceController.cmd
+++ StartSqlServiceController.cmd
@@ -1,6 +1,7 @@
 @echo off
 rem #****************************************
-rem #*** Script created for iSoft Systems ***
-rem #***   by Jim Calano 9/14/2024        ***
+rem #*** Start the app as Administrator   ***
 rem #****************************************
 PowerShell -Command "Start-Process PowerShell -ArgumentList 'C:\Users\Public\Documents\SqlServerServicesController.ps1' -Verb RunAs"
+rem******************************************
+rem #* SQL Server Services Controller © 2024 by Jim Calano is licensed under CC BY-NC-SA 4.0 
